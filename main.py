0299d390a27f10973e8c86576787d38fb6203890
andreeaflorescu <andreea.florescu15@gmail.com>
2017-01-17 00:39:21 +0200

    added main

---
+++ main.py
@@ -0,0 +1,14 @@
+import scipy
+import peakutils
+import sys
+from song import Song, WAVSong, WAVSongSplitter
+sys.path.insert(0, 'modules/genreXpose/genreXpose')
+from tester import test_model_on_single_file
+
+import warnings
+warnings.filterwarnings('ignore')
+
+test_model_on_single_file(sys.argv[1])
+song = WAVSong(sys.argv[1])
+song.plot_frequencies()
+song.split_in_musical_phrases()
